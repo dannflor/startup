365da30b4e6396b95c82e3ef9b50f7b777465f3e
ethanmchls <ethanmchls@gmail.com>
2023-01-11 22:30:07 -0700

    first commit

--- tradeworld/Sources/App/configure.swift
+++ tradeworld/Sources/App/configure.swift
@@ -7,6 +7,8 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    let file = FileMiddleware (publicDirectory: app.directory.publicDirectory)
+    app.middleware.use(file)
 
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
