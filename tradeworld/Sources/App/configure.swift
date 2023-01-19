b57727c815c61aa580713cee3d8eb20df86aa47f
Sam Bishop <sambobjr@gmail.com>
2023-01-19 13:36:21 -0700

    Fix postgres connection

--- tradeworld/Sources/App/configure.swift
+++ tradeworld/Sources/App/configure.swift
@@ -9,7 +9,7 @@ public func configure(_ app: Application) throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     let file = FileMiddleware (publicDirectory: app.directory.publicDirectory)
     app.middleware.use(file)
-    app.databases.use(try! .postgres(url: Environment.get("DB_CONN_STRING")!), as: .psql)
+    app.databases.use(try! .postgres(url: Environment.get("DATABASE_URL")!), as: .psql)
     
 
     app.migrations.add(CreateTodo())
