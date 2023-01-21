4a77da1ff7fac164ec2e032503e1af7ce8845f1d
ethanmchls <ethanmchls@gmail.com>
2023-01-21 10:59:26 -0700

    added login page

--- tradeworld/Sources/App/routes.swift
+++ tradeworld/Sources/App/routes.swift
@@ -2,8 +2,12 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
+    app.get { req in
+        req.redirect(to: "/login")
+    }
+    
+    app.get("login") { req async throws in
+        try await req.view.render("login")
     }
 
     app.get("hello") { req async -> String in
