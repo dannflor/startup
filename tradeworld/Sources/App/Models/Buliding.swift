3c44a80a0a98b4507b4104b93a0f5202906d4e28
ethanmchls <ethanmchls@gmail.com>
2023-03-23 22:09:35 -0600

    Building.getNeighbors function with empty neighbors

--- tradeworld/Sources/App/Models/Buliding.swift
+++ tradeworld/Sources/App/Models/Buliding.swift
@@ -3,109 +3,109 @@ import Vapor
 enum Building: Content {
     case Academy, Armory, Blacksmith, Carpenter, Cottage, Estate, Farm01, Farm02, Fishery, Forest, Fort, Fortress, Guild, LargeHouse, LumberCamp, MediumHouse, Mine, Mountain, NoHouse, Palace, Pasture, Quarry, Quarry02, SmallHouse, Tower, University, Water, Watermill, Windmill
     
-    static func getNeighbors(index: Int) -> [Int] {
+    static func getNeighbors(index: Int) -> (Int?, Int?, Int?, Int?) {
         switch (index) {
             case 0:
-              return [1, 2];
+              return (nil, nil, 1, 2);
             case 1:
-              return [0, 3, 4];
+              return (nil, 0, 3, 4);
             case 2:
-              return [0, 4, 5];
+              return (0, nil, 4, 5);
             case 3:
-              return [1, 6, 7];
+              return (nil, 1, 6, 7);
             case 4:
-              return [1, 2, 7, 8];
+              return (1, 2, 7, 8);
             case 5:
-              return [2, 8, 9];
+              return (2, nil, 8, 9);
             case 6:
-              return [3, 10, 11];
+              return (nil, 3, 10, 11);
             case 7:
-              return [3, 4, 11, 12];
+              return (3, 4, 11, 12);
             case 8:
-              return [4, 5, 12, 13];
+              return (4, 5, 12, 13);
             case 9:
-              return [5, 13, 14];
+              return (5, nil, 13, 14);
             case 10:
-              return [6, 15, 16];
+              return (nil, 6, 15, 16);
             case 11:
-              return [6, 7, 16, 17];
+              return (6, 7, 16, 17);
             case 12:
-              return [7, 8, 17, 18];
+              return (7, 8, 17, 18);
             case 13:
-              return [8, 9, 18, 19];
+              return (8, 9, 18, 19);
             case 14:
-              return [9, 19, 20];
+              return (9, nil, 19, 20);
             case 15:
-              return [10, 21, 22];
+              return (nil, 10, 21, 22);
             case 16:
-              return [10, 11, 22, 23];
+              return (10, 11, 22, 23);
             case 17:
-              return [11, 12, 23, 24];
+              return (11, 12, 23, 24);
             case 18:
-              return [12, 13, 24, 25];
+              return (12, 13, 24, 25);
             case 19:
-              return [13, 14, 25, 26];
+              return (13, 14, 25, 26);
             case 20:
-              return [14, 26, 27];
+              return (14, nil, 26, 27);
             case 21:
-              return [15, 28];
+              return (nil, 15, nil, 28);
             case 22:
-              return [15, 16, 28, 29];
+              return (15, 16, 28, 29);
             case 23:
-              return [16, 17, 29, 30];
+              return (16, 17, 29, 30);
             case 24:
-              return [17, 18, 30, 31];
+              return (17, 18, 30, 31);
             case 25:
-              return [18, 19, 31, 32];
+              return (18, 19, 31, 32);
             case 26:
-              return [19, 20, 32, 33];
+              return (19, 20, 32, 33);
             case 27:
-              return [20, 33];
+              return (20, nil, 33, nil);
             case 28:
-              return [21, 22, 34];
+              return (21, 22, nil, 34);
             case 29:
-              return [22, 23, 34, 35];
+              return (22, 23, 34, 35);
             case 30:
-              return [23, 24, 35, 36];
+              return (23, 24, 35, 36);
             case 31:
-              return [24, 25, 36, 37];
+              return (24, 25, 36, 37);
             case 32:
-              return [25, 26, 37, 38];
+              return (25, 26, 37, 38);
             case 33:
-              return [26, 27, 38];
+              return (26, 27, 38, nil);
             case 34:
-              return [28, 29, 39];
+              return (28, 29, nil, 39);
             case 35:
-              return [29, 30, 39, 40];
+              return (29, 30, 39, 40);
             case 36:
-              return [30, 31, 40, 41];
+              return (30, 31, 40, 41);
             case 37:
-              return [31, 32, 41, 42];
+              return (31, 32, 41, 42);
             case 38:
-              return [32, 33, 42];
+              return (32, 33, 42, nil);
             case 39:
-              return [34, 35, 43];
+              return (34, 35, nil, 43);
             case 40:
-              return [35, 36, 43, 44];
+              return (35, 36, 43, 44);
             case 41:
-              return [36, 37, 44, 45];
+              return (36, 37, 44, 45);
             case 42:
-              return [37, 38, 45];
+              return (37, 38, 45, nil);
             case 43:
-              return [39, 40, 46];
+              return (39, 40, nil, 46);
             case 44:
-              return [40, 41, 46, 47];
+              return (40, 41, 46, 47);
             case 45:
-              return [41, 42, 47];
+              return (41, 42, 47, nil);
             case 46:
-              return [43, 44, 48];
+              return (43, 44, nil, 48);
             case 47:
-              return [44, 45, 48];
+              return (44, 45, 48, nil);
             case 48:
-              return [46, 47];
+              return (46, 47, nil, nil);
           
             default:
-              return [0];
-          }
+              return (nil, nil, nil, nil);
+        }
     }
 }
