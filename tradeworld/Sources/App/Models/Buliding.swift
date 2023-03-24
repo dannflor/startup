40a2639427d1ff56b0c70add34b6186a473b08f2
ethanmchls <ethanmchls@gmail.com>
2023-03-23 21:45:11 -0600

    Building enum

--- tradeworld/Sources/App/Models/Buliding.swift
+++ tradeworld/Sources/App/Models/Buliding.swift
@@ -1,5 +1,5 @@
 import Vapor
 
 enum Building: Content {
-    
+    case Academy, Armory, Blacksmith, Carpenter, Cottage, Estate, Farm01, Farm02, Fishery, Forest, Fort, Fortress, Guild, LargeHouse, LumberCamp, MediumHouse, Mine, Mountain, NoHouse, Palace, Pasture, Quarry, Quarry02, SmallHouse, Tower, University, Water, Watermill, Windmill
 }
