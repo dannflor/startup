7526a8101ee5279b4db506aa9fdd7e8bdcdf799c
Sam Bishop <sambobjr@gmail.com>
2023-04-18 22:26:33 -0600

    AsyncMap

--- tradeworld/Sources/App/routes.swift
+++ tradeworld/Sources/App/routes.swift
@@ -175,3 +175,17 @@ func decodeFile<T: Decodable>(req: Request, _ file: String, _ type: T.Type) -> T
     }
     return resource
 }
+
+extension Sequence {
+    func asyncMap<T>(
+        _ transform: (Element) async throws -> T
+    ) async rethrows -> [T] {
+        var values = [T]()
+
+        for element in self {
+            try await values.append(transform(element))
+        }
+
+        return values
+    }
+}
