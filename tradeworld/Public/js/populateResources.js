9ebf1df5cd7efdc912f71404f6e97872213439df
Sam Bishop <sambobjr@gmail.com>
2023-04-19 12:42:24 -0600

    Add icons to various places

--- tradeworld/Public/js/populateResources.js
+++ tradeworld/Public/js/populateResources.js
@@ -9,18 +9,23 @@ export async function populateResources() {
   let i = 0;
   resources.forEach(resource => {
     const div = document.createElement('div');
-    div.setAttribute('class', 'flex-1 flex md:space-x-2 bg-base-100 py-3 rounded-lg mx-1 my-1 px-1 tooltip tooltip-bottom');
+    div.setAttribute('class', 'flex-1 flex md:space-x-2 py-3 rounded-lg mx-1 my-1 px-1 tooltip tooltip-bottom');
     div.setAttribute('data-tip', '+' + resourceYields[i].count + ' per hour');
     const innerDiv = document.createElement('div');
-    innerDiv.setAttribute('class', 'mx-auto md:flex-row md:flex md:text-base text-xs');
-    const name = document.createElement('div');
-    name.setAttribute('class', 'text-info font-bold md:mr-1 text-center md:text-left');
-    name.textContent = resource.name + ':';
+    innerDiv.setAttribute('class', 'mx-auto md:flex-row md:flex text-base');
+    const imageContainer = document.createElement('div');
+    imageContainer.setAttribute('class', 'w-10');
+    const image = document.createElement('img');
+    // Set name to lowercase to match image name
+    image.setAttribute('src', '/img/icon/' + resource.name.toLowerCase() + '.png');
+    image.setAttribute('class', 'w-full h-auto block');
+    image.setAttribute('style', 'image-rendering: pixelated; image-rendering: crisp-edges;');
+    imageContainer.appendChild(image);
+    innerDiv.appendChild(imageContainer);
     const count = document.createElement('div');
-    count.setAttribute('class', 'text-neutral-content text-center md:text-left');
+    count.setAttribute('class', 'text-info font-bold text-center md:ml-2 md:text-left my-auto');
     count.setAttribute('id', 'resource' + resource.name);
     count.textContent = resource.count;
-    innerDiv.appendChild(name);
     innerDiv.appendChild(count);
     div.appendChild(innerDiv);
     resourceDiv.insertBefore(div, scoreDiv);
