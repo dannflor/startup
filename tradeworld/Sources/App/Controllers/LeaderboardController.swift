e89bf1253f5e290fc6dbf9618cd221d01b364a04
ethanmchls <ethanmchls@gmail.com>
2023-04-18 22:29:11 -0600

    async mapping for leaderboard

--- tradeworld/Sources/App/Controllers/LeaderboardController.swift
+++ tradeworld/Sources/App/Controllers/LeaderboardController.swift
@@ -9,14 +9,15 @@ func leaderboardController(leaderboard: RoutesBuilder) {
     
     leaderboard.get(":page") { req async throws -> View in
         let page = Int(req.parameters.get("page") ?? "1") ?? 1
-        let users = try await User.query(on: req.db).all().map { user in
-            LeaderboardEntry(username: user.username, score: user.getScore(req), resource: user.$resources.get(on: req.db) ?? Resource())
+        let users = try await User.query(on: req.db).all().asyncMap { user in
+            try await LeaderboardEntry(username: user.username, score: user.getScore(req), resource: user.$resources.get(on: req.db) ?? Resource())
         }.sorted { 
             // Sort by score and then by resource
-            $0.score > $1.score || ($0.score == $1.score && $0.resource > $1.resource)
+            // || ($0.score == $1.score && $0.resource > $1.resource)
+            $0.score > $1.score
         }
         
-        let context = LeaderboardContext(users: users.items, page: page)
+        let context = LeaderboardContext(users: users, page: page)
         return try await req.view.render("leaderboard", context)
     }
 }
